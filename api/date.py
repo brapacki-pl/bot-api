9814a7025d5ff35ac644347cf6a2ad74c68950dc
brapacki-pl <33712314+brapacki-pl@users.noreply.github.com>
2020-07-06 17:26:56 +0200

    json response

--- api/date.py
+++ api/date.py
@@ -1,11 +1,12 @@
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
+import json
 
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
+    self.send_header('Content-type', 'application/json')
     self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    self.wfile.write(json.dumps({'4': 5, '6': 7}, sort_keys=True, indent=4))
     return
