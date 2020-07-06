5e381677ad2ec81a42f0717a4af89e72d44971a3
brapacki-pl <33712314+brapacki-pl@users.noreply.github.com>
2020-07-06 17:56:04 +0200

    Update date.py

--- api/date.py
+++ api/date.py
@@ -6,7 +6,7 @@ class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     self.send_response(200)
-    self.send_header('Content-type', 'application/json')
+    self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(json.dumps({'4': 5, '6': 7}, sort_keys=True, indent=4))
+    self.wfile.write("Hello world")
     return
